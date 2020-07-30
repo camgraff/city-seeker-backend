61a5fc3092dcfd2a224844b7360521b7bf2eab81
Cam <graffcameron@gmail.com>
2020-07-29 21:16:10 -0500

    Implemented logic for users joining a game

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
-const express = require('express');
-const app = express();
-const port = 3001;
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+require('dotenv').config();
+const app = require('express')();
+const PORT = 3000;
+const server = require('http').createServer(app);
+const io = require('socket.io')(server); 
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials", true);
+    next();
+});
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
 io.on('connection', socket => {
     console.log('a user connected');
+    socket.on('joinGame', (gameId, username) => {
+        handleUserJoined(socket, gameId, username);
+    });
 });
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+function handleUserJoined(socket, gameId, username) {
+    socket.username = username;
+    io.in(gameId).clients((error, clients) => {
+        console.log(clients);
+        let sockets = io.sockets.sockets;
+        clients.forEach(client => {
+            socket.emit('userJoined', sockets[client].username);
+        });
+    });
+    socket.join(gameId, () => {
+        console.log(username +' joined');
+        socket.to(gameId).emit('userJoined', username);
+    });
+}
+
+server.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`));
